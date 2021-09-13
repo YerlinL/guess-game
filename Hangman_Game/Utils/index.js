09c4469eab55d62f02b0590a05b12d298cd0e06e
DESKTOP-E9C33I4\Yerlin <ledemadrigal@gmail.com>
2021-09-12 18:33:29 -0600

    remove sp

--- Hangman_Game/Utils/index.js
+++ Hangman_Game/Utils/index.js
@@ -1,14 +1,13 @@
-const sp = require('synchronized-promise');
-
 function generarNumeroAleatorio(Maximo){
     let aleatorio= Math.floor(Math.random()*Maximo);
     return aleatorio;
 }
+const wait = time => {
+    const start = Date.now();
+    while(Date.now()-start < time){}
+}
 
-const sleep = time => new Promise(resolve =>setTimeout(resolve,time))
-const wait = sp(sleep);
-
-export default {
+module.exports = {
     generarNumeroAleatorio,
     wait
 }
