606648e87b893d961920108ea2e738b85cdce4a7
ebrenes-dev <ebrenes@soin.co.cr>
2022-01-06 22:04:40 -0600

    audios funcionales

--- Hangman_Game/Views/js/Constantes.js
+++ Hangman_Game/Views/js/Constantes.js
@@ -1,153 +1,232 @@
 export default {
-    ocupaciones: [
-        "baker", "butcher", 
-        "salesman", "saleswoman", 
-        "cashier", "hairdresser", 
-        "mechanic", "chef", 
-        "farmer", "waiter", 
-        "waitress", "bank", 
-        "school", "church",
-        "post office", "restaurant",
-        "bakery", "drugstore",
-        "grocery store", "butchery",
-        "hardware store", "shoe store", 
-        "clothing store", "bookstore"
-    ],
-    estadosPartida:{
-        gano:'gano',
-        perdio:'perdio',
-        continua:'continua'
+  ocupaciones: [
+    "baker",
+    "butcher",
+    "salesman",
+    "saleswoman",
+    "cashier",
+    "hairdresser",
+    "mechanic",
+    "chef",
+    "farmer",
+    "waiter",
+    "waitress",
+    "bank",
+    "school",
+    "church",
+    "post office",
+    "restaurant",
+    "bakery",
+    "drugstore",
+    "grocery store",
+    "butchery",
+    "hardware store",
+    "shoe store",
+    "clothing store",
+    "bookstore",
+  ],
+  estadosPartida: {
+    gano: "gano",
+    perdio: "perdio",
+    continua: "continua",
+  },
+  estadoJuego: {
+    finalizado: "finalizo",
+    continuaJuego: "continuaJuego",
+  },
+  pistas: {
+    baker: "This person works in a bakery",
+    butcher: "This person works in a butcher shop",
+    salesman: "This person is a man who works in a store",
+    saleswoman: "This person is a woman who works in a store",
+    cashier: "This person works in a supermarket",
+    hairdresser: "This person works in a beauty salon",
+    mechanic: "This person works in a mechanical workshop",
+    chef: "This person works in a restaurant",
+    farmer: "This person has cows, pigs and hens",
+    waiter:
+      "This person is a man who brings us the menu when we go to a restaurant",
+    waitress:
+      "This person is a women who brings us the menu when we go to a restaurant",
+    bank: "In this place people deposit their money",
+    school: "The teacher works in this place",
+    church: "In this place people pray",
+    "post office":
+      "This place is in charge of collecting and sending your letters",
+    restaurant: "The chef works in this place",
+    bakery: "The baker works in this place",
+    drugstore: "In this place you can buy medicines",
+    "grocery store": "In this place you can buy candies, sugar and rice",
+    butchery: "In this place you can buy meat",
+    "hardware store": "In this place you can buy a hammer",
+    "shoe store": "In this place people buy shoes",
+    "clothing store": "In this place people buy cloth",
+    bookstore: "In this place people buy books",
+  },
+  oraciones: {
+    baker: {
+      palabra: "The word is baker",
+      oracion: "The baker bakes cakes",
     },
-    estadoJuego:{
-        finalizado: 'finalizo',
-        continuaJuego: 'continuaJuego'
+    butcher: {
+      palabra: "The word is butcher",
+      oracion: "The butcher sells meat",
     },
-    pistas:{
-        "baker": 'This person works in a bakery',
-        "butcher": 'This person works in a butcher shop',
-        "salesman": 'This person is a man who works in a store',
-        "saleswoman": 'This person is a woman who works in a store',
-        "cashier": 'This person works in a supermarket',
-        "hairdresser": 'This person works in a beauty salon',
-        "mechanic": 'This person works in a mechanical workshop',
-        "chef": 'This person works in a restaurant',
-        "farmer": 'This person has cows, pigs and hens',
-        "waiter": 'This person is a man who brings us the menu when we go to a restaurant',
-        "waitress": 'This person is a women who brings us the menu when we go to a restaurant',
-        "bank": 'In this place people deposit their money',
-        "school": 'The teacher works in this place',
-        "church": 'In this place people pray',
-        "post office": 'This place is in charge of collecting and sending your letters',
-        "restaurant": 'The chef works in this place',
-        "bakery": 'The baker works in this place',
-        "drugstore": 'In this place you can buy medicines',
-        "grocery store": 'In this place you can buy candies, sugar and rice',
-        "butchery": 'In this place you can buy meat',
-        "hardware store": 'In this place you can buy a hammer',
-        "shoe store": 'In this place people buy shoes',
-        "clothing store": 'In this place people buy cloth',
-        "bookstore": 'In this place people buy books'
-
-
+    salesman: {
+      palabra: "The word is salesman",
+      oracion: "The salesman sells products",
     },
-    oraciones:{
-        "baker":{
-            palabra: 'The word is baker', 
-            oracion: 'The baker bakes cakes'
-        },
-        "butcher":{
-            palabra: 'The word is butcher',
-            oracion: 'The butcher sells meat'
-        },
-        "salesman": {
-            palabra: 'The word is salesman',
-            oracion: 'The salesman sells products'
-        },
-        "saleswoman": {
-            palabra: 'The word is saleswoman',
-            oracion: 'The saleswoman sells products '
-        },
-        "cashier": {
-            palabra: 'The word is cashier',
-            oracion: 'The cashier receives money from the customers'
-        },
-        "hairdresser": {
-            palabra:'The word is hairdresser',
-            oracion:'The hairdresser cuts the hair'
-        },
-        "mechanic": {
-            palabra:'The word is mechanic ',
-            oracion:'The mechanic repairs cars'
-        },
-        "chef": {
-            palabra:'The word is chef',
-            oracion:'The chef cooks food'
-        },
-        "farmer": {
-            palabra:'The word is farmer',
-            oracion:'The farmer plants corn '
-        },
-        "waiter": {
-            palabra:'The word is waiter',
-            oracion:'The waiter serves the food in the restaurant'
-        },
-        "waitress": {
-            palabra:'The word is waitress',
-            oracion:'The waitress serves the food in the restaurant'
-        },
-        "bank": {
-            palabra:'The word is bank',
-            oracion:'The clerk works in a bank'
-        },
-        "school": {
-            palabra:'The word is school',
-            oracion:'The teacher works in a school '
-        },
-        "church": {
-            palabra:'The word is church',
-            oracion:'People go to church every Sunday'
-        },
-        "post office": {
-            palabra:'The word is post office',
-            oracion:'The letter is in the post office'
-        },
-        "restaurant": {
-            palabra:'The word is restaurant',
-            oracion:'They eat dinner at the restaurant'
-        },
-        "bakery": {
-            palabra:'The word us bakery',
-            oracion:'We go to the bakery to buy bread'
-        },
-        "drugstore": {
-            palabra:'The word is drug store',
-            oracion:'People go to the drug store to buy medicines'
-        },
-        "grocery store": {
-            palabra:'The word is grocery store',
-            oracion:'Children like to buy candies at the grocery store'
-        },
-        "butchery": {
-            palabra:'The word is butchery',
-            oracion:'The chef goes to the butchery to buy meat for lunch'
-        },
-        "hardware store": {
-            palabra:'The word is hardware store ',
-            oracion:'The farmer goes to the hardware store to buy nails'
-        },
-        "shoe store": {
-            palabra:'The word is shoe store',
-            oracion:'The saleswoman sells shoes in the shoe store'
-        },
-        "clothing store": {
-            palabra:'',
-            oracion:''
-        },
-        "bookstore": {
-            palabra:'The word is book store',
-            oracion:'The salesman sells story books in the bookstore'
-        }
-
-
-    }
-}
+    saleswoman: {
+      palabra: "The word is saleswoman",
+      oracion: "The saleswoman sells products ",
+    },
+    cashier: {
+      palabra: "The word is cashier",
+      oracion: "The cashier receives money from the customers",
+    },
+    hairdresser: {
+      palabra: "The word is hairdresser",
+      oracion: "The hairdresser cuts the hair",
+    },
+    mechanic: {
+      palabra: "The word is mechanic ",
+      oracion: "The mechanic repairs cars",
+    },
+    chef: {
+      palabra: "The word is chef",
+      oracion: "The chef cooks food",
+    },
+    farmer: {
+      palabra: "The word is farmer",
+      oracion: "The farmer plants corn ",
+    },
+    waiter: {
+      palabra: "The word is waiter",
+      oracion: "The waiter serves the food in the restaurant",
+    },
+    waitress: {
+      palabra: "The word is waitress",
+      oracion: "The waitress serves the food in the restaurant",
+    },
+    bank: {
+      palabra: "The word is bank",
+      oracion: "The clerk works in a bank",
+    },
+    school: {
+      palabra: "The word is school",
+      oracion: "The teacher works in a school ",
+    },
+    church: {
+      palabra: "The word is church",
+      oracion: "People go to church every Sunday",
+    },
+    "post office": {
+      palabra: "The word is post office",
+      oracion: "The letter is in the post office",
+    },
+    restaurant: {
+      palabra: "The word is restaurant",
+      oracion: "They eat dinner at the restaurant",
+    },
+    bakery: {
+      palabra: "The word us bakery",
+      oracion: "We go to the bakery to buy bread",
+    },
+    drugstore: {
+      palabra: "The word is drug store",
+      oracion: "People go to the drug store to buy medicines",
+    },
+    "grocery store": {
+      palabra: "The word is grocery store",
+      oracion: "Children like to buy candies at the grocery store",
+    },
+    butchery: {
+      palabra: "The word is butchery",
+      oracion: "The chef goes to the butchery to buy meat for lunch",
+    },
+    "hardware store": {
+      palabra: "The word is hardware store ",
+      oracion: "The farmer goes to the hardware store to buy nails",
+    },
+    "shoe store": {
+      palabra: "The word is shoe store",
+      oracion: "The saleswoman sells shoes in the shoe store",
+    },
+    "clothing store": {
+      palabra: "",
+      oracion: "",
+    },
+    bookstore: {
+      palabra: "The word is book store",
+      oracion: "The salesman sells story books in the bookstore",
+    },
+  },
+  audios: {
+    baker: { pista: "./audios/prueba.mp3", oracion: "prueba" },
+    butcher: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    salesman: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    saleswoman: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    cashier: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    hairdresser: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    mechanic: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    chef: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    farmer: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    waiter: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    waitress: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    bank: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    school: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    church: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    "post office": {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    restaurant: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    bakery: { pista: "./audios/prueba.mp3", oracion: "./audios/prueba.mp3" },
+    drugstore: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    "grocery store": {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    butchery: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    "hardware store": {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    "shoe store": {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    "clothing store": {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+    bookstore: {
+      pista: "./audios/prueba.mp3",
+      oracion: "./audios/prueba.mp3",
+    },
+  },
+};
--- Hangman_Game/Views/js/Formulario.js
+++ Hangman_Game/Views/js/Formulario.js
@@ -1,194 +1,177 @@
-import ElementoHTML from './ElementoHTMLBuilder.js'; 
+import ElementoHTML from "./ElementoHTMLBuilder.js";
 import GeneradorPartidas from "./GeneradorPartidas.js";
-import Constantes from './Constantes.js'
-import Utils from './Utils/index.js'
-
-
+import Constantes from "./Constantes.js";
+import Utils from "./Utils/index.js";
 
 const BOTON_DESHABILIDADO = "btn btn-info disabled";
 const BOTON_HABILITADO = "btn btn-info";
 const ID_AUDIO = "pistaAudio";
-const { estadosPartida, pistas } = Constantes;
+const { estadosPartida, pistas, audios } = Constantes;
 const generadorPartidas = new GeneradorPartidas();
-let { partida,palabra } = generadorPartidas.generarPartida();
+let { partida, palabra } = generadorPartidas.generarPartida();
+let audio = null;
 
+function iniciar() {
+  generarAbecedario();
+  crearFormulario(palabra.length);
+  asignarPista(palabra);
+}
 
+function crearFormulario(cantidadLetrasPalabraSeleccionada) {
+  let formulario = document.getElementById("input");
+  const altura = "50px";
+  const ancho = "50px";
+  const desactivado = true;
+  for (
+    let cantidadCuadrosTexto = 0;
+    cantidadCuadrosTexto < cantidadLetrasPalabraSeleccionada;
+    ++cantidadCuadrosTexto
+  ) {
+    let elementoNuevo = document.createElement("input");
+    const elemento = new ElementoHTML(elementoNuevo)
+      .establecerAltura(altura)
+      .establecerAncho(ancho)
+      .establecerID(cantidadCuadrosTexto)
+      .establecerClase(palabra[cantidadCuadrosTexto])
+      .establecerEstado(desactivado);
+    let input = elemento.obtenerElemento();
+    formulario.appendChild(input);
+  }
+}
 
+function generarAbecedario() {
+  const altura = "55px";
+  const ancho = "60px";
+  const colorFondo = "#ff6565";
+  const colorBorde = "solid 1px #a94a50";
+  let a = 97;
+  let z = 122;
+  let g = 103;
+  let n = 110;
+  let u = 117;
+  let abecedario = document.getElementById("abecedario");
+  let espacio = document.createTextNode("\u00a0");
 
-function iniciar(){
-    generarAbecedario(); 
+  for (let letra = a; letra <= z; ++letra) {
+    let elementoNuevo = document.createElement("button");
+    let letraCreada = String.fromCharCode(letra);
+    const boton = new ElementoHTML(elementoNuevo)
+      .establecerTipo("button")
+      .establecerClase("btn btn-info")
+      .establecerAltura(altura)
+      .establecerAncho(ancho)
+      .establecerColorFondo(colorFondo)
+      .establecerColorBorde(colorBorde);
+    const botonNuevo = boton.obtenerElemento();
+    botonNuevo.innerText = letraCreada;
+    botonNuevo.onclick = adivinar;
+    abecedario.appendChild(botonNuevo);
+    abecedario.appendChild(espacio);
+
+    if (letra === g || letra === n || letra === u) {
+      agregarCambioLinea(abecedario);
+    }
+  }
+}
+
+function adivinar() {
+  this.className = BOTON_DESHABILIDADO;
+  const letraSeleccionada = this.innerText;
+  mostrarLetra(letraSeleccionada);
+  const { estado } = partida.actualizarEstadoDePartida(letraSeleccionada);
+  if (estado === estadosPartida.gano) {
+    //alert("Congratulations you win!, try with another word");
+    mostrarModal();
+    regenerarPartida();
+  } else if (estado === estadosPartida.perdio) {
+    alert("You lost, good luck next time, try again");
+    regenerarPartida();
+  }
+}
+
+function regenerarPartida() {
+  borrarFormulario();
+  habilitarBotones();
+  const partidaGenerada = generadorPartidas.generarPartida();
+  partida = partidaGenerada.partida;
+  palabra = partidaGenerada.palabra;
+  if (partida !== null) {
     crearFormulario(palabra.length);
     asignarPista(palabra);
+  } else {
+    alert("Game finished");
+  }
 }
 
-function crearFormulario(cantidadLetrasPalabraSeleccionada){ 
-    let formulario = document.getElementById("input");
-    const altura = '50px';
-    const ancho = '50px';
-    const desactivado= true;
-    for(let cantidadCuadrosTexto= 0; cantidadCuadrosTexto < cantidadLetrasPalabraSeleccionada; ++cantidadCuadrosTexto){
-
-        let elementoNuevo = document.createElement('input');
-        const elemento = new ElementoHTML(elementoNuevo)
-                        .establecerAltura(altura)
-                        .establecerAncho(ancho)
-                        .establecerID(cantidadCuadrosTexto)
-                        .establecerClase(palabra[cantidadCuadrosTexto])
-                        .establecerEstado(desactivado);
-        let input = elemento.obtenerElemento();
-        formulario.appendChild(input);
-        
-    }    
-    
+function borrarFormulario() {
+  const padre = document.getElementById("input");
+  while (padre.firstChild) {
+    padre.removeChild(padre.firstChild);
+  }
 }
-
-function generarAbecedario(){
-    const altura = '55px';
-    const ancho = '60px';
-    const colorFondo = '#ff6565';
-    const colorBorde = 'solid 1px #a94a50';
-    let a = 97;
-    let z = 122;
-    let g = 103;
-    let n = 110;
-    let u = 117;
-    let abecedario = document.getElementById("abecedario");
-    let espacio = document.createTextNode("\u00a0");
-    
-    for (let letra = a; letra <= z; ++letra){
-                
-        let elementoNuevo = document.createElement('button');
-        let letraCreada = String.fromCharCode(letra);
-        const boton = new ElementoHTML(elementoNuevo)
-                        .establecerTipo("button")
-                        .establecerClase("btn btn-info")
-                        .establecerAltura(altura)
-                        .establecerAncho(ancho)
-                        .establecerColorFondo(colorFondo)
-                        .establecerColorBorde(colorBorde);
-        const botonNuevo  = boton.obtenerElemento(); 
-        botonNuevo.innerText= letraCreada;
-        botonNuevo.onclick = adivinar;
-        abecedario.appendChild(botonNuevo);
-        abecedario.appendChild(espacio);
-        
-        if(letra===g || letra===n || letra===u ){
-            agregarCambioLinea(abecedario);
-            
-        }
-        
+function habilitarBotones() {
+  const padre = document.getElementById("abecedario");
+  const botonesAbecedario = padre.childNodes;
+  for (let boton of botonesAbecedario) {
+    if (boton.className === BOTON_DESHABILIDADO) {
+      boton.className = BOTON_HABILITADO;
     }
-    
+  }
 }
 
-function adivinar(){
-    this.className=BOTON_DESHABILIDADO;
-    const letraSeleccionada = this.innerText;
-    mostrarLetra(letraSeleccionada);
-    const { estado } = partida.actualizarEstadoDePartida(letraSeleccionada);
-    if(estado === estadosPartida.gano){
-        //alert("Congratulations you win!, try with another word");
-        mostrarModal();
-        regenerarPartida();
-    }else if(estado === estadosPartida.perdio){
-        alert("You lost, good luck next time, try again");
-        regenerarPartida();
-    }
+function agregarCambioLinea(elemento) {
+  let cambioLinea = document.createElement("br");
+  elemento.appendChild(cambioLinea);
 }
 
-function regenerarPartida(){
-    borrarFormulario();
-    habilitarBotones();
-    const partidaGenerada = generadorPartidas.generarPartida();
-    partida = partidaGenerada.partida;
-    palabra = partidaGenerada.palabra;
-    if(partida!==null){
-        crearFormulario(palabra.length);
-        asignarPista(palabra);
-    }else{
-        alert("Game finished");
-    }
+function mostrarLetra(letraSeleccionada) {
+  let cajas = document.getElementsByClassName(letraSeleccionada);
+  for (let caja of cajas) {
+    caja.value = letraSeleccionada;
+    caja.style.color = "#a94a50";
+    caja.style.textAlign = "center";
+    caja.style.fontWeight = "bold";
+  }
 }
 
-function borrarFormulario(){
-    const padre = document.getElementById("input");
-    while(padre.firstChild){
-        padre.removeChild(padre.firstChild);
-    }
-}
-function habilitarBotones(){
-    const padre = document.getElementById("abecedario");
-    const botonesAbecedario = padre.childNodes;
-    for (let boton of botonesAbecedario){
-        if(boton.className === BOTON_DESHABILIDADO){
-            boton.className = BOTON_HABILITADO;
-        }
-    }
-
-
+function asignarPista(palabra) {
+  const tarjetaTexto = document.getElementById("pista");
+  const tarjetaCarta = document.getElementById("pista-card");
+  tarjetaTexto.innerText = pistas[palabra];
+  tarjetaCarta.onmouseover = generarReproducirAudio(palabra);
+  tarjetaCarta.onmouseout = detenerAudio;
 }
 
-function agregarCambioLinea(elemento){
-    let cambioLinea = document.createElement("br");
-    elemento.appendChild(cambioLinea);
+function generarReproducirAudio(palabra) {
+  const rutaAudio = audios[palabra].pista;
+  return () => {
+    audio = new Audio(rutaAudio);
+    audio.id = ID_AUDIO;
+    audio.play();
+  };
 }
 
-function mostrarLetra(letraSeleccionada){
-    let cajas = document.getElementsByClassName(letraSeleccionada);
-    for(let caja of cajas){
-        caja.value=letraSeleccionada;
-        caja.style.color = '#a94a50';
-        caja.style.textAlign = 'center';
-        caja.style.fontWeight = 'bold';
-    }
+function detenerAudio() {
+  if (audio != null) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
 }
 
-function asignarPista(palabra){
-    const tarjeta = document.getElementById("pista");
-    tarjeta.innerText = pistas[palabra];
-   // tarjeta.onmouseover = generarReproducirAudio(palabra);
-    //tarjeta.onmouseout = detenerAudio();
-    
-   
+function mostrarModal() {
+  alert("Mostrar modal");
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  cerrarModal();
 }
 
-function generarReproducirAudio(palabra){
-    const rutaAudio = './audios/prueba.mp3';
-    return()=>{
-       const audio = new Audio(rutaAudio);
-       audio.id = ID_AUDIO;
-       audio.play();
-    }
-   
+function cerrarModal() {
+  var span = document.getElementsByClassName("close")[0];
+  var modal = document.getElementById("myModal");
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
 }
-
-/*function detenerAudio(){
-    const audio = document.getElementById(ID_AUDIO);
-    if(audio!=null){
-        audio.pause(); 
-        audio.currentTime=0;
-    }
-    
-    
-}*/
-
-function mostrarModal(){
-    alert("Mostrar modal");
-    var modal = document.getElementById("myModal");
-    modal.style.display = "block";
-    cerrarModal();
-    
-}
-
-function cerrarModal(){
-    var span = document.getElementsByClassName("close")[0];
-    var modal = document.getElementById("myModal");
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-}
-/*Ventana modal https://www.w3schools.com/howto/howto_css_modals.asp*/ 
+/*Ventana modal https://www.w3schools.com/howto/howto_css_modals.asp*/
 
 iniciar();
-
-
